0c7278512b790b8f56f614ea289c008ed0c3940a
Leonardo Briseño <hectorspacejam@gmail.com>
2018-11-14 09:34:14 -0600

    finished app

--- src/pages/add/add.ts
+++ src/pages/add/add.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
+import { IonicPage, NavController, NavParams, AlertController } from 'ionic-angular';
+import { HomePage } from '../home/home';
 /**
  * Generated class for the AddPage page.
  *
@@ -15,11 +15,37 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class AddPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  home=HomePage;
+
+  contacto={
+    name: "",
+    mail: "",
+    cel: "",
+    fb: "",
+    twitter: "",
+    inst: "",
+    img: "../assets/imgs/02.jpg"
+  }
+
+  constructor(public navCtrl: NavController, public navParams: NavParams,
+    public alertCtrl: AlertController) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad AddPage');
+  }
+
+  sendHome(){
+    if(this.contacto.name!=""&&this.contacto.cel!=""){
+    this.navCtrl.push(this.home,this.contacto);
+    }else{
+      const alert=this.alertCtrl.create({
+      title: "Oh no!",
+      subTitle: "Necesitas escribir un nombre y teléfono",
+      buttons: ["Ok"]
+      });
+      alert.present();
+    }
   }
 
 }
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, NavParams } from 'ionic-angular';
 
 import { AddPage } from '../add/add';
 import { ContactoPage } from '../contacto/contacto';
@@ -23,8 +23,27 @@ export class HomePage {
      img: "../assets/imgs/hgtt.jpg"
    } 
   ];
-  constructor(public navCtrl: NavController) {
+  temp={
+    name: "",
+    mail: "",
+    cel: "",
+    fb: "",
+    twitter: "",
+    inst: "",
+    img: ""
+  };
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+    this.temp.name=this.navParams.get('name');
+    this.temp.mail=this.navParams.get('mail');
+    this.temp.cel=this.navParams.get('cel');
+    this.temp.fb=this.navParams.get('fb');
+    this.temp.twitter=this.navParams.get('twitter');
+    this.temp.inst=this.navParams.get('inst');
+    this.temp.img=this.navParams.get('img');
 
+    if(this.temp.name!=null){
+      this.contactos.push(this.temp);
+    }
   }
   addContact(){
     this.navCtrl.push(this.add);
